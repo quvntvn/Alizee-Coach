a6956722afd4c7122ed02dbc578447892eb0e913
quvntvn <hulkufe4@gmail.com>
2024-11-08 12:27:07 +0100

    maj header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,48 @@
-// src/components/Header.tsx
+"use client";
 
+import { useState } from 'react';
 import styles from './style/Header.module.css';
 import Link from "next/link";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);  // Ferme le menu lorsqu'un lien est cliqué
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <Link href="/">
           <img src="images/logo.png" alt="Thé Tip Top Logo" className={styles.logo} />
         </Link>
-        <nav className={styles.navMenu}>
-        <Link href="/" legacyBehavior>
-            <a className={styles.navLink}>Accueil</a>
+        
+        {/* Bouton pour le menu mobile */}
+        <button className={styles.menuButton} onClick={toggleMenu}>
+          ☰
+        </button>
+
+        {/* Menu navigation */}
+        <nav className={`${styles.navMenu} ${menuOpen ? styles.navMenuOpen : ''}`}>
+          <Link href="/" legacyBehavior>
+            <a className={styles.navLink} onClick={closeMenu}>Accueil</a>
           </Link>
           <Link href="/services" legacyBehavior>
-            <a className={styles.navLink}>Services</a>
+            <a className={styles.navLink} onClick={closeMenu}>Services</a>
           </Link>
           <Link href="/temoignages" legacyBehavior>
-            <a className={styles.navLink}>Témoignages</a>
+            <a className={styles.navLink} onClick={closeMenu}>Témoignages</a>
           </Link>
           <Link href="/a-propos" legacyBehavior>
-            <a className={styles.navLink}>À propos</a>
+            <a className={styles.navLink} onClick={closeMenu}>À propos</a>
           </Link>
           <Link href="/contact" legacyBehavior>
-            <a className={styles.navLink}>Contact</a>
+            <a className={styles.navLink} onClick={closeMenu}>Contact</a>
           </Link>
         </nav>
       </div>
