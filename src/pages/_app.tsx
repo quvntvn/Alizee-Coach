5af6abb9bfee95d03b965d9dbed679b41a562daf
quvntvn <hulkufe4@gmail.com>
2024-11-08 12:01:59 +0100

    maj accueil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app';
 import '../app/globals.css';
+import styles from '../components/style/Footer.module.css'; // Assure-toi que le chemin est correct
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return (
+    <div className={styles.pageWrapper}> {/* Ajout du wrapper */}
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
