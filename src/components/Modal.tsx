90e6b27d8d73d729457941acc4e4685d5b36e8c4
quvntvn <hulkufe4@gmail.com>
2024-11-29 15:08:06 +0100

    maj image

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,7 @@
-import React, { ReactNode } from "react";
+'use client';
+
+import React, { ReactNode, useState } from "react";
+import Image from "next/image"; // Importer Image pour le chargement
 import styles from "./style/Modal.module.css";
 
 interface ModalProps {
@@ -8,6 +11,8 @@ interface ModalProps {
 }
 
 export default function Modal({ isOpen, onClose, children }: ModalProps) {
+  const [imageLoaded, setImageLoaded] = useState(false); // État pour le chargement de l'image
+
   if (!isOpen) return null;
 
   return (
@@ -16,7 +21,17 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
         <button className={styles.closeButton} onClick={onClose}>
           &times;
         </button>
-        <div className={styles.modalBody}>{children}</div>
+        <div className={styles.modalBody}>
+          {/* Ajout d'un spinner pour l'image dans la popup */}
+          {!imageLoaded && <div className={styles.spinner}></div>}
+          <div className={styles.imageContainer}>
+            {/* Ajout de l'image avec le suivi du chargement */}
+            {children && React.cloneElement(children as React.ReactElement, {
+              onLoad: () => setImageLoaded(true), // Détecte le chargement de l'image
+              className: `${styles.image} ${imageLoaded ? "" : styles.hidden}` // Masque l'image avant qu'elle ne soit chargée
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
